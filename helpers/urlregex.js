35f33f7ca9cb7463d0210f7a560197f1b1645a3d
mburuanthony <mburuantony@outlook.com>
2023-12-14 01:08:39 +0300

    regex utility

---
+++ helpers/urlregex.js
@@ -0,0 +1,7 @@
+export const qrDataIsUrl = (scanResult) => {
+  var expression =
+    /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+  var regex = new RegExp(expression);
+
+  return scanResult.match(regex) ? true : false;
+};
