4a320be3cfb9d77719b73087b2569b317d11dc5e
mburuanthony <mburuantony@outlook.com>
2023-12-14 01:06:51 +0300

    modified App

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import "expo-dev-client";
 import { useCallback } from "react";
 import { StatusBar } from "expo-status-bar";
 import { useFonts } from "expo-font";
@@ -6,13 +5,14 @@ import * as ExpoSplashScreen from "expo-splash-screen";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { ScanHistoryProvider } from "./context/scanhistoryctx";
 import HomeScreen from "./screens/HomeScreen";
 import Scanner from "./screens/Scanner";
 
 const Stack = createNativeStackNavigator();
 ExpoSplashScreen.preventAutoHideAsync();
 
-export default function App() {
+function App() {
   const [fontsLoaded] = useFonts({
     "ops-light": require("./assets/fonts/OpenSans-Light.ttf"),
     "ops-regular": require("./assets/fonts/OpenSans-Regular.ttf"),
@@ -32,13 +32,35 @@ export default function App() {
   return (
     <SafeAreaProvider onLayout={onLayoutRootView}>
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+            animation: "default",
+            animationTypeForReplace: "pop",
+          }}
+        >
           <Stack.Screen name="home" component={HomeScreen} />
-          <Stack.Screen name="scan" component={Scanner} />
+          <Stack.Screen
+            name="scan"
+            component={Scanner}
+            options={{
+              animation: "slide_from_bottom",
+              animationTypeForReplace: "push",
+              animationDuration: 25,
+            }}
+          />
         </Stack.Navigator>
 
         <StatusBar style="dark" animated={true} backgroundColor="transparent" />
       </NavigationContainer>
     </SafeAreaProvider>
+  );
+}
+
+export default function AppProvider() {
+  return (
+    <ScanHistoryProvider>
+      <App />
+    </ScanHistoryProvider>
   );
 }
