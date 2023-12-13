d995ca76bfd211374bd5a18ae09796a79241606d
mburuanthony <mburuantony@outlook.com>
2023-12-14 01:07:49 +0300

    shared styles

--- assets/sharedstyles.js
+++ assets/sharedstyles.js
@@ -2,23 +2,24 @@ import { colors } from "./colors";
 
 export const container = {
   flex: 1,
+  paddingHorizontal: 8,
   backgroundColor: colors.white,
 };
 
 export const text = {
-  fontSize: 14,
-  fontfamily: "ops-regular",
+  fontSize: 12,
+  fontFamily: "ops-regular",
   color: colors.primary,
 };
 
 export const textlight = {
-  fontSize: 14,
-  fontfamily: "ops-light",
+  fontSize: 12,
+  fontFamily: "ops-light",
   color: colors.primary,
 };
 
 export const title = {
-  fontSize: 18,
-  fontfamily: "ops-semibold",
+  fontSize: 20,
+  fontFamily: "ops-semibold",
   color: colors.primary,
 };
