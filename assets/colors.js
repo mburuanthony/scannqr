64f6579a99b96b7e0f33c3c386bbf2915e3c7a83
mburuanthony <mburuantony@outlook.com>
2023-12-14 01:07:36 +0300

    colors

--- assets/colors.js
+++ assets/colors.js
@@ -4,6 +4,7 @@ export const colors = {
   white: "#FFFFFF",
   divider: "rgba(163, 208, 162, 0.55)",
   dividerdark: "rgba(163, 208, 162, 0.2)",
+  dividerlight: "rgba(248, 250, 252, 0.2)",
   skyblue: "rgba(168, 209, 237, 0.08)",
   skybluedark: "rgba(168, 209, 237, 0.04)",
 };
